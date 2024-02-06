a4629f628bff032978b5bcb53515f2dfa70a0455
Tony Field <tony@kingsway.digital>
2024-02-06 18:38:47 -0500

    Multiple teleop updates - Initialize rotation2d to 0 to avoid jumping to an old position on startup - reworked invert code to use a boolean - updated pov formula to reflect x positive zero - updated smartdashboard logging - moved pid wrapper to computeOmega ( no performance cost) - IMPORTANT: in driveFieldOriented, negated x, y, w to fix orientation. THis is not yet undertood.
    Co-developed with Nathan.

--- src/main/java/frc/robot/commands/swervedrive/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/TeleopDriveCommand.java
@@ -18,7 +18,7 @@ public class TeleopDriveCommand extends LoggingCommand {
 
     private final SwerveSubsystem swerve;
     private final OperatorInput   oi;
-    private Rotation2d            prevTheta = null;
+    private Rotation2d            prevTheta = Rotation2d.fromDegrees(0);
 
     /**
      * Used to drive a swerve robot in full field-centric mode.
@@ -41,54 +41,47 @@ public class TeleopDriveCommand extends LoggingCommand {
 
         // The FRC field-oriented ccoordinate system
         // https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html
-        Alliance alliance             = getAlliance().orElse(Alliance.Blue);
+        final Alliance alliance             = getAlliance().orElse(Alliance.Blue);
 
         // The coordinate system defines (0,0) as the right side of the blue alliance wall. The
         // x-axis is positive toward the red alliance, and the y-axis is positive to the left.
         // When the robot is on the red alliance, we need to invert inputs for the stick values
         // to move the robot in the right direction.
-        int      invertFactor         = alliance == Alliance.Blue ? 1 : -1;
+        final boolean  invert               = alliance == Alliance.Red;
 
         // With the driver standing behind the driver station glass, "forward" on the left stick is
         // its y value, but that should convert into positive x movement on the field. The
         // Runnymede Controller inverts stick y-axis values, so "forward" on stick is positive.
         // Thus, positive y stick axis maps to positive x translation on the field.
-        double   vX                   = oi.getDriverControllerAxis(LEFT, Y);
+        final double   vX                   = oi.getDriverControllerAxis(LEFT, Y);
 
         // Left and right movement on the left stick (the stick's x-axis) maps to the y-axis on the
         // field. Left on the stick (negative x) maps to positive y on the field, and vice versa.
         // Thus, negative x stick axis maps to positive y translation on the field.
-        double   vY                   = -oi.getDriverControllerAxis(LEFT, X);
+        final double   vY                   = -oi.getDriverControllerAxis(LEFT, X);
 
         // Left and right on the right stick will change the direction the robot is facing - its
         // heading. Positive x values on the stick translate to clockwise motion, and vice versa.
         // The coordinate system has positive motion as CCW.
         // Therefore, negative x stick value maps to positive rotation on the field.
-        double   ccwRotAngularVelPct  = -oi.getDriverControllerAxis(RIGHT, X);
+        final double   ccwRotAngularVelPct  = -oi.getDriverControllerAxis(RIGHT, X);
 
         // User wants to jump directly to a specific heading. Computation is deferred because it is
         // complex
         // and may not be necessary. See below for details.
-        int      rawDesiredHeadingDeg = oi.getPOV();
+        final int      rawDesiredHeadingDeg = oi.getPOV();
 
         // Compute boost factor
-        boolean  isSlow               = oi.isDriverLeftBumper();
-        boolean  isFast               = oi.isDriverRightBumper();
-        double   boostFactor          = isSlow ? SLOW_SPEED_FACTOR : (isFast ? MAX_SPEED_FACTOR : GENERAL_SPEED_FACTOR);
-
-        // write to dashboard
-        SmartDashboard.putNumber("vX", vX);
-        SmartDashboard.putNumber("vY", vY);
-        SmartDashboard.putNumber("ccwRotAngularVelPct", ccwRotAngularVelPct);
-        SmartDashboard.putNumber("rawDesiredHeadingDeg", rawDesiredHeadingDeg);
-        SmartDashboard.putNumber("boostFactor", boostFactor);
+        final boolean  isSlow               = oi.isDriverLeftBumper();
+        final boolean  isFast               = oi.isDriverRightBumper();
+        final double   boostFactor          = isSlow ? SLOW_SPEED_FACTOR : (isFast ? MAX_SPEED_FACTOR : GENERAL_SPEED_FACTOR);
 
 
-        Translation2d translation = new Translation2d(
-            Math.pow(vX, 3) * boostFactor * MAX_TRANSLATION_SPEED_MPS * invertFactor,
-            Math.pow(vY, 3) * boostFactor * MAX_TRANSLATION_SPEED_MPS * invertFactor);
+        Translation2d  translation          = new Translation2d(
+            Math.pow(vX, 3) * boostFactor * MAX_TRANSLATION_SPEED_MPS * (invert ? -1 : 1),
+            Math.pow(vY, 3) * boostFactor * MAX_TRANSLATION_SPEED_MPS * (invert ? -1 : 1));
 
-        Rotation2d    omega;
+        Rotation2d     omega;
 
         // User is steering!
         if (ccwRotAngularVelPct != 0) {
@@ -103,24 +96,32 @@ public class TeleopDriveCommand extends LoggingCommand {
             // POV coordinates don't match field coordinates. POV is CW+ and field is CCW+. Also,
             // POV 0 is 90 degrees on the field (for blue alliance, and -90 for red).
             // Invert and rotate as required.
-            // BLUE field = mod( (pov * -1) + 90 + 360, 360)
-            // RED field = mod( (pov * -1) - 90 + 360, 360)
-            double     correctedDegrees = ((rawDesiredHeadingDeg * -1) + (invertFactor * 90) + 360) % 360;
-            Rotation2d desiredHeading   = Rotation2d.fromDegrees(correctedDegrees);
+            // BLUE field = MOD(-POV + 360, 360)
+            // RED field = MOD(-POV + 180 + 360, 360)
+            double correctedHeadingDeg = ((rawDesiredHeadingDeg * -1) + (invert ? 180 : 0) + 360) % 360;
+            SmartDashboard.putNumber("Teleop/correctedHeadingDeg", correctedHeadingDeg);
+            Rotation2d desiredHeading = Rotation2d.fromDegrees(correctedHeadingDeg);
             omega     = swerve.computeOmega(desiredHeading);
             // Save the previous heading for when the jump is done
             prevTheta = desiredHeading;
         }
         else {
             // Translating only. If we know our previous heading, keep it and don't change it.
-            // If we never set a specific heading before, just use the current one.
-            if (prevTheta == null) {
-                prevTheta = swerve.getPose().getRotation();
-            }
-            omega = swerve.computeOmega(prevTheta);
+            prevTheta = swerve.getPose().getRotation();
+            omega     = swerve.computeOmega(prevTheta);
         }
 
-        SmartDashboard.putString("Drive ", translation + " m/s " + omega);
+        // write to dashboard
+        SmartDashboard.putString("Teleop/Alliance", alliance.name());
+        SmartDashboard.putNumber("Teleop/vX", vX);
+        SmartDashboard.putNumber("Teleop/vY", vY);
+        SmartDashboard.putNumber("Teleop/ccwRotAngularVelPct", ccwRotAngularVelPct);
+        SmartDashboard.putNumber("Teleop/rawDesiredHeadingDeg", rawDesiredHeadingDeg);
+        SmartDashboard.putNumber("Teleop/boostFactor", boostFactor);
+
+        SmartDashboard.putString("Teleop/Translation", translation.getNorm() + "m/s at " + translation.getAngle());
+        SmartDashboard.putString("Teleop/Theta ", prevTheta.getDegrees() + " deg");
+        SmartDashboard.putString("Teleop/Omega", omega.getDegrees() + " deg/sec");
         swerve.driveFieldOriented(translation, omega);
 
     }
