02f40c0f371e4d41b51446098eb02a8119437dd1
Tony Field <tony@kingsway.digital>
2024-02-10 18:25:56 -0500

    Fix typo

--- src/main/java/frc/robot/subsystems/vision/VisionPositionInfo.java
+++ src/main/java/frc/robot/subsystems/vision/VisionPositionInfo.java
@@ -2,5 +2,5 @@ package frc.robot.subsystems.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
 
-public record VisionPositionInfo(Pose2d posse, long targetingLatencyMillis, long captureLatencyMillis) {
+public record VisionPositionInfo(Pose2d pose, long targetingLatencyMillis, long captureLatencyMillis) {
 }
