230b84b7a6d8672a1411c5fec386ebfb2749cd6e
Quentin Field <q@quentinfield.com>
2024-02-17 15:39:19 -0500

    create Score4Speaker auto - partially pseudocode

---
+++ src/main/java/frc/robot/commands/auto/Score4SpeakerAutoCommand.java
@@ -0,0 +1,89 @@
+package frc.robot.commands.auto;
+
+import static edu.wpi.first.wpilibj.DriverStation.getAlliance;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.BotTarget;
+import frc.robot.commands.swervedrive.DriveDistanceCommand;
+import frc.robot.commands.swervedrive.DriveRobotOrientedCommand;
+import frc.robot.commands.swervedrive.ResetOdometryCommand;
+import frc.robot.commands.swervedrive.RotateToTargetCommand;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+import frc.robot.subsystems.vision.HughVisionSubsystem;
+
+public class Score4SpeakerAutoCommand extends SequentialCommandGroup {
+
+    public Score4SpeakerAutoCommand(SwerveSubsystem swerve, HughVisionSubsystem hugh) {
+
+        final Alliance  alliance = getAlliance().orElse(Alliance.Blue);
+        final int       invert;
+        final BotTarget wolverine;
+        final BotTarget barnum;
+        final BotTarget valjean;
+        final BotTarget speaker;
+
+        if (alliance == Alliance.Blue) {
+            wolverine = BotTarget.BLUE_WOLVERINE;
+            barnum    = BotTarget.BLUE_BARNUM;
+            valjean   = BotTarget.BLUE_VALJEAN;
+            speaker   = BotTarget.BLUE_SPEAKER;
+            invert    = 1;
+        }
+        // Red Alliance
+        else {
+            wolverine = BotTarget.RED_WOLVERINE;
+            barnum    = BotTarget.RED_BARNUM;
+            valjean   = BotTarget.RED_VALJEAN;
+            speaker   = BotTarget.RED_SPEAKER;
+            invert    = -1;
+        }
+
+
+
+        // TODO: implement Auto Selector
+        Rotation2d startingRotation = Rotation2d.fromDegrees(0);
+        Pose2d     startingPose     = new Pose2d(1.37, 5.55, startingRotation);
+        // Configure
+        addCommands(new ResetOdometryCommand(swerve, startingPose));
+
+        /* ***AUTO PATTERN*** */
+
+        /* Note 1 */
+        // addCommands(new ScoreSpeakerCommand());
+
+        addCommands(new DriveDistanceCommand(swerve, new Translation2d(invert * 0.25, 0), new Rotation2d(), 0.1));
+
+        /* Note 2 */
+        addCommands(new RotateToTargetCommand(swerve, hugh, wolverine));
+
+        // addCommands(new VisionNotePickup());
+        addCommands(new DriveRobotOrientedCommand(swerve, new Translation2d(.1, 0), new Rotation2d()));
+        addCommands(new RotateToTargetCommand(swerve, hugh, speaker));
+        // addCommands(new ScoreSpeakerCommand());
+
+
+        /* Note 3 */
+        addCommands(new RotateToTargetCommand(swerve, hugh, barnum));
+
+        // addCommands(new VisionNotePickup());
+        addCommands(new DriveRobotOrientedCommand(swerve, new Translation2d(.1, 0), new Rotation2d()));
+        addCommands(new RotateToTargetCommand(swerve, hugh, speaker));
+        // addCommands(new ScoreSpeakerCommand());
+
+        /* Note 4 */
+        addCommands(new RotateToTargetCommand(swerve, hugh, valjean));
+
+        // addCommands(new VisionNotePickup());
+        addCommands(new DriveRobotOrientedCommand(swerve, new Translation2d(.1, 0), new Rotation2d()));
+        addCommands(new RotateToTargetCommand(swerve, hugh, speaker));
+        // addCommands(new ScoreSpeakerCommand());
+
+        /* Exit Zone */
+        addCommands(new DriveDistanceCommand(swerve, new Translation2d(invert * 1, 0), Rotation2d.fromDegrees(0), 1));
+
+    }
+}
