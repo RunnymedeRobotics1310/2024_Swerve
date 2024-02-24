d7d960aa6272f684487ecc27022dddde93257dd6
Jeff Zakrzewski <jeff@zakr.ca>
2024-02-24 13:14:48 -0500

    Add distance to detected April Tag Info.  Cleanup dashboard output of tags.

--- src/main/java/frc/robot/subsystems/vision/AprilTagInfo.java
+++ src/main/java/frc/robot/subsystems/vision/AprilTagInfo.java
@@ -1,4 +1,4 @@
 package frc.robot.subsystems.vision;
 
-public record AprilTagInfo(int tagId, double xAngle, double xTrans, double yTrans) {
+public record AprilTagInfo(int tagId, double xAngle, double xTrans, double yTrans, double targetDistance) {
 }
